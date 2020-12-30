b4c357d424506cc0e2f5b94ddf928e1f8bbb5fb6
Archana <diyaarchu1995@gmail.com>
2020-12-30 11:56:01 +0530

    phone number screen

---
+++ app/src/main/java/com/example/hinge/phone_num.java
@@ -0,0 +1,14 @@
+package com.example.hinge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class phone_num extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_num);
+    }
+}
