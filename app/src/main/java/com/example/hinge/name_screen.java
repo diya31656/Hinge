b93873cf7166baa35b60e5913a0b22f4b218d1a0
Archana <diyaarchu1995@gmail.com>
2020-12-30 19:03:22 +0530

    name screen

---
+++ app/src/main/java/com/example/hinge/name_screen.java
@@ -0,0 +1,14 @@
+package com.example.hinge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class name_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_screen);
+    }
+}
