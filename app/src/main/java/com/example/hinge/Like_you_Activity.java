8eebb47f80640217b03adb20523d08bdebceff29
Nipun Jain <lucifernipun22@gmail.com>
2020-12-31 17:11:06 +0530

    "Like_you_screen"

---
+++ app/src/main/java/com/example/hinge/Like_you_Activity.java
@@ -0,0 +1,14 @@
+package com.example.hinge;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Like_you_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_like_you_);
+    }
+}
--- app/src/main/java/com/example/hinge/MainActivity.java
+++ app/src/main/java/com/example/hinge/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         initviews();
-        
+
 
 
         mTvTermsofService.setOnClickListener(v -> urlOpner("https://hinge.co/terms.html"));
