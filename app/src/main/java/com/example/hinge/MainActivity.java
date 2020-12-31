0d4b37719d26be76db46e8de90c008ffb92b7259
SpiderGod607 <karthikm607@gmail.com>
2020-12-31 14:15:44 +0530

    main activity

--- app/src/main/java/com/example/hinge/MainActivity.java
+++ app/src/main/java/com/example/hinge/MainActivity.java
@@ -25,9 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         initviews();
 
-        Intent intent = new Intent(MainActivity.this,DiscoverActivity.class);
-        startActivity(intent);
-
         mTvTermsofService.setOnClickListener(v -> urlOpner("https://hinge.co/terms.html"));
         mTvPrivacyPolicy.setOnClickListener(v -> urlOpner("https://hinge.co/privacy.html"));
 
