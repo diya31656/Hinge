cfa47b4c8dae72fb6d7193621d89dcc78569b91f
SpiderGod607 <karthikm607@gmail.com>
2021-01-01 13:20:35 +0530

    add changes to settings screen

--- app/src/main/java/com/example/hinge/Roses_activity.java
+++ app/src/main/java/com/example/hinge/Roses_activity.java
@@ -1,14 +1,42 @@
 package com.example.hinge;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class Roses_activity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_roses_activity);
+
+        initView();
     }
+
+    ImageView ivmCross;
+
+    private void initView() {
+        ivmCross = findViewById(R.id.ivCross);
+        setListeners();
+
+    }
+
+    private void setListeners() {
+        ivmCross.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
 }
